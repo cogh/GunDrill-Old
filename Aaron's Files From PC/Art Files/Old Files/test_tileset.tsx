1a91ceca7617bba0269dceba47eb1b796f0a07e0
cogh <coghdevelops@gmail.com>
2019-04-14 16:31:43 +0100

    Deleting all the files
    Deleting all the files so we can start fresh (current files are old versions and we haven't organised together yet).

--- Aaron's Files From PC/Art Files/Old Files/test_tileset.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset name="Test" tilewidth="8" tileheight="8" tilecount="4608" columns="256">
- <image source="New Tiles.png" width="2048" height="144"/>
-</tileset>
